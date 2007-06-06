d43620786bb6d78dbaf034b9912ca6e009f57a8e
FeepingCreature <FeepingCreature@d50c83ef-0632-0410-bad2-33ec4536786f>
2007-06-06 19:24:23 +0000

    fix

--- src/freelink.d
+++ src/freelink.d
@@ -1,10 +1,10 @@
-import computer, file;
+import computer, file, nls;
 import std.stdio;
 import SDL;
 
 void main ()
 {
-  writefln ("FreeLink");
+  writefln (nl("freelink"));
   File f = new File (1, "Test.txt", 3, 0, false);
   Computer x = new Computer (0, "Localhost");
   writefln (x.name);
