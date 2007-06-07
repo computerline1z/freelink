3c3a621b5668c81bab98ddd3802015f9583e35ca
omouse <omouse@d50c83ef-0632-0410-bad2-33ec4536786f>
2007-06-07 00:13:18 +0000

    Added gui.d

---
+++ src/gui.d
@@ -0,0 +1,48 @@
+module gui;
+import SDL, png;
+
+class Widget {
+  SDL_Rect r;
+  void draw (SDL_Surface *) {
+  }
+}
+import std.file;
+class Window : Widget {
+  char[] title;
+  SDL_Surface *titleBar;
+  bool dragging;
+
+  this (char[] title, int x, int y, uint width, uint height) {
+    this.title = title;
+    this.r.x = x;
+    this.r.y = y;
+    this.r.w = width;
+    this.r.h = height;
+    this.titleBar = decode (cast(ubyte[])read("titlebar.png"));
+  }
+
+  void startDrag () {
+    dragging = true;
+  }
+  void stopDrag (int x, int y) {
+    dragging = false;
+    this.r.x = x;
+    this.r.y = y;
+  }
+  void draw (SDL_Surface *surf) {
+    if (dragging) {
+    } else {
+      SDL_BlitSurface (titleBar, &r, surf, &r);
+      putpixel (surf, r.x, r.y, [255, 255, 255]);
+      putpixel (surf, r.x+r.w, r.y, [255, 255, 255]);
+      putpixel (surf, r.x+r.w, r.y+r.h, [255, 255, 255]);
+      putpixel (surf, r.x, r.y+r.h, [255, 255, 255]);
+    }
+  }
+}
+
+class Button : Widget {
+  char[] caption;
+  void draw (SDL_Surface *surf) {
+  }
+}
