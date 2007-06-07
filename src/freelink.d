c8a818d6187c18a33f7fafc3fa8c505ac3bc8dd9
omouse <omouse@d50c83ef-0632-0410-bad2-33ec4536786f>
2007-06-07 01:16:20 +0000

    blah

--- src/freelink.d
+++ src/freelink.d
@@ -16,7 +16,7 @@ void main ()
   writefln (f.name);
   writefln ("Available space: ", x.space.available);
 
-  Window testWindow = new Window ("Test", 10, 10, 10, 20);
+  Window testWindow = new Window ("Test", 10, 10, 48, 48);
 
   SDL_Surface *screen = SDL_SetVideoMode (640, 480, 32, SDL_SWSURFACE);
   SDL_Event event;
