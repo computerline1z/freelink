54c882f2fa532370f8916f18d74f6fc8567cf6a3
FeepingCreature <FeepingCreature@d50c83ef-0632-0410-bad2-33ec4536786f>
2007-06-07 18:19:31 +0000

    Frame!

--- src/gui.d
+++ src/gui.d
@@ -12,6 +12,7 @@ class FileSource {
  
 class Area {
   SDL_Rect me; SDL_Surface *mine;
+  int w() { return me.w; } int h() { return me.h; }
   this(SDL_Rect  r, SDL_Surface *s) { me=r; mine=s; }
   static Area opCall(SDL_Surface *s, SDL_Rect *r=null) {
     if (!r) {
@@ -20,10 +21,17 @@ class Area {
     }
     return new Area(*r, s);
   }
-  int blit (SDL_Surface *surf, ushort x, ushort y) { /// blit surf on me at x, y
+  int blit (SDL_Surface *surf, int x, int y, int w=0, int h=0) {
+    return blit(surf, cast(ushort)x, cast(ushort)y, cast(ushort)w, cast(ushort)h);
+  }
+  int blit (SDL_Surface *surf, ushort x, ushort y, ushort w=0, ushort h=0) { /// blit surf on me at x, y
     SDL_Rect dest = void;
+    if (!w) w=cast(ushort)surf.w; if (!h) h=cast(ushort)surf.h;
     dest.x=cast(short)x; dest.y=cast(short)y;
-    return SDL_BlitSurface (surf, null, mine, &dest);
+    dest.w=w; dest.h=h;
+    SDL_Rect src=dest;
+    src.x=0; src.y=0;
+    return SDL_BlitSurface (surf, &src, mine, &dest);
   }
 }
 
@@ -57,10 +65,17 @@ class Button : Widget {
   }
 }
 
+long eatoi(char[] nr, int max) {
+  assert(nr.length);
+  if (nr[$-1]=='%') return (max*atoi(nr[0..$-1]))/100;
+  return atoi(nr);
+}
+
 import xml, util, std.string;
 class Frame : Widget {
   FileSource fsrc;
-  Widget[char[]] parts;
+  SDL_Surface *[char[]] parts;
+  ubyte[] modes; invariant { assert(modes.length==4); }
   /// generate a SDL surface from an XML description
   private SDL_Surface *getSurface(xmlElement thingie) {
     SDL_Surface *res=null;
@@ -74,15 +89,13 @@ class Frame : Widget {
           assert("from" in tag.attributes, "Error: part without from");
           assert("to" in tag.attributes, "Error: part without to");
           assert(tag.children.length==1, "Error: No surface below "~tag.toString);
-          auto supersurf=getSurface(tag.children[0]);
+          auto supersurf=getSurface(tag.children[0]); scope(exit) if (supersurf) SDL_FreeSurface(supersurf);
           // rectangle strings
           auto r_str=split(tag.attributes["from"], ",")~split(tag.attributes["to"], ",");
           foreach (inout text; r_str) text=strip(text);
-          long[4] rect; // top-x, top-y, bottom-x, bottom-y
-          if (r_str[0][$-1]=='%') rect[0]=(supersurf.w*atoi(r_str[0][0..$-1]))/100; else rect[0]=atoi(r_str[0]);
-          if (r_str[1][$-1]=='%') rect[1]=(supersurf.h*atoi(r_str[1][0..$-1]))/100; else rect[1]=atoi(r_str[1]);
-          if (r_str[2][$-1]=='%') rect[2]=(supersurf.w*atoi(r_str[2][0..$-1]))/100-1; else rect[2]=atoi(r_str[2])-1;
-          if (r_str[3][$-1]=='%') rect[3]=(supersurf.h*atoi(r_str[3][0..$-1]))/100-1; else rect[3]=atoi(r_str[3])-1;
+          // top-x, top-y, bottom-x, bottom-y
+          long[4] rect;
+          with (*supersurf) rect[]=[eatoi(r_str[0], w), eatoi(r_str[1], h), eatoi(r_str[2], w), eatoi(r_str[3], h)];
           res=MakeSurf(rect[2]-rect[0], rect[3]-rect[1], 32);
           SDL_Rect source=void;
           with (source) {
@@ -98,7 +111,7 @@ class Frame : Widget {
         case "rotate":
           assert("mode" in tag.attributes, "Error: rotate without mode");
           assert(tag.children.length==1, "Error: No surface below "~tag.toString);
-          auto supersurf=getSurface(tag.children[0]); // get surface above
+          auto supersurf=getSurface(tag.children[0]); scope(exit) SDL_FreeSurface(supersurf);
           auto mode=tag.attributes["mode"];
           switch (mode) {
             case "right":
@@ -114,7 +127,7 @@ class Frame : Widget {
             case "180":
               res=MakeSurf(supersurf.w, supersurf.h, 32);
               for (int x=0; x<res.w; ++x) for (int y=0; y<res.h; ++y)
-                putpixel(res, x, y, getpixel(supersurf, supersurf.w-x-1, supersurf.h-y-1));
+               putpixel(res, x, y, getpixel(supersurf, supersurf.w-x-1, supersurf.h-y-1));
               break;
             default: assert(false, "Unknown rotate mode: "~mode);
           }
@@ -129,54 +142,60 @@ class Frame : Widget {
     this.fsrc=fsrc;
     assert(frame.name=="frame", "Frame tag data not actually frame");
     xmlElement[char[]] entries;
-    char[][char[]] modes;
+    char[][char[]] modestr;
     foreach (_ch; frame.children) {
       ifIs(_ch, (xmlTag ch) {
         assert(ch.children.length==1, "Invalid number of children in "~ch.toString);
         entries[ch.name]=ch.children[0];
-        assert("mode" in ch.attributes, "No mode specification in "~ch.toString);
-        modes[ch.name]=ch.attributes["mode"];
+        if ("mode" in ch.attributes) modestr[ch.name]=ch.attributes["mode"];
       });
     }
     // extract the SDL surfaces for the stuffies
-    SDL_Surface*[char[]] surfs;
-    foreach (name, tree; entries) surfs[name]=getSurface(tree);
+    foreach (name, tree; entries) parts[name]=getSurface(tree);
     
     // use the modes to determine the border sizes
-    int[][char[]] sizes; // top-left, top, top-right, left, right, bottom-left, bottom, right
-    foreach (name, mode; modes) {
-      char[] xmode, ymode;
-      if (mode.find(",")!=-1) {
-        auto sp=mode.split(",");
-        assert(sp.length==2, "Property error in mode "~mode);
-        xmode=sp[0]; ymode=sp[1];
-      } else xmode=ymode=mode;
-      xmode=strip(xmode); ymode=strip(ymode);
-      // mode can be fixed, repeat, stretch ... only fixed locks the size
-      ubyte[char[]] match; match=["fixed".dup: 1, "repeat": 0, "stretch": 0];
-      assert(xmode in match, "Unknown mode: "~xmode);
-      assert(ymode in match, "Unknown mode: "~ymode);
-      assert(name in surfs, "Unknown surface: "~name);
-      sizes[name]=[[-1, surfs[name].w][match[xmode]], [-1, surfs[name].h][match[ymode]]];
+    foreach (n; ["top-left", "top-right", "bottom-left", "bottom-right"])
+      assert(!(n in modestr), "Error: The size of corners is determined by the sides");
+    foreach (n; ["top", "left", "right", "bottom"])
+      assert(n in modestr, "Error: Side "~n~" : Scale mode not defined");
+    void mustBeEqual(int a, int b, int c) {
+      assert((a==b)&&(a==c), format("Error: Sizes ", a, ", ", b, ", ", c, " unequal!"));
     }
-    // now use the sizes to determine the width of top, left, right and bottom border.
-    int getSize(int arrayOffs, char[][] comps...) {
-      // all fixed components must have the same size, and there must be at least one fixed component.
-      auto sizes=map(comps, (char[] c) { assert(c in sizes, "Warning: size "~c~" does not exist"); return sizes[c][arrayOffs]; });
-      int size=-1;
-      foreach (s; sizes)
-        if (size!=-1) {
-          if (s!=-1)
-            assert(s==size, "Size conflict: fixed "~.toString(s)~" vs fixed "~.toString(size));
-        } else size=s;
-      assert(size!=-1, format("Size error: components ", comps, ", being ", sizes, ", do not define a size"));
-      return size;
-    }
-    auto bordersizes=[getSize(1, "top-left", "top", "top-right"), getSize(0, "top-left", "left", "bottom-left"),
-      getSize(0, "top-right", "right", "bottom-right"), getSize(1, "bottom-left", "bottom", "bottom-right")];
-    writefln("Border sizes: ", bordersizes);
-    std.c.stdlib.exit(0);
+    mustBeEqual(parts["top-left"].h, parts["top"].h, parts["top-right"].h);
+    mustBeEqual(parts["bottom-left"].h, parts["bottom"].h, parts["bottom-right"].h);
+    mustBeEqual(parts["top-left"].w, parts["left"].w, parts["bottom-left"].w);
+    mustBeEqual(parts["top-right"].w, parts["right"].w, parts["bottom-right"].w);
+    static ubyte[char[]] map; if (!map.keys.length)
+      map=["repeat".dup: 0, "stretch": 1];
+    modes=[map[modestr["top"]], map[modestr["left"]], map[modestr["right"]], map[modestr["bottom"]]];
   }
   void draw(Area target) {
+    // draw the frame
+    target.blit(parts["top-left"], 0, 0);
+    target.blit(parts["top-right"], target.w-parts["top-right"].w, 0);
+    target.blit(parts["bottom-left"], 0, target.h-parts["bottom-left"].h);
+    target.blit(parts["bottom-right"], target.w-parts["bottom-right"].w, target.h-parts["bottom-right"].h);
+    
+    foreach (m; modes) assert(m==0, r"\todo: implement stretching");
+    
+    // draw horizontal stripes
+    int offs=parts["top-left"].w;
+    while (offs+parts["top"].w<target.w-parts["top-right"].w) {
+      target.blit(parts["top"], offs, 0);
+      target.blit(parts["bottom"], offs, target.h-parts["bottom"].h);
+      offs+=parts["top"].w;
+    }
+    target.blit(parts["top"], offs, 0, target.w-parts["top-right"].w-offs, parts["top"].h);
+    target.blit(parts["bottom"], offs, target.h-parts["bottom"].h, target.w-parts["top-right"].w-offs, parts["top"].h);
+    
+    // draw vertical stripes
+    offs=parts["top-left"].h;
+    while (offs+parts["left"].h<target.h-parts["bottom-left"].h) {
+      target.blit(parts["left"], 0, offs);
+      target.blit(parts["right"], target.w-parts["right"].w, offs);
+      offs+=parts["left"].h;
+    }
+    target.blit(parts["left"], 0, offs, parts["left"].w, target.h-parts["bottom-left"].h-offs);
+    target.blit(parts["right"], target.w-parts["right"].w, offs, parts["left"].w, target.h-parts["bottom-left"].h-offs);
   }
 }
